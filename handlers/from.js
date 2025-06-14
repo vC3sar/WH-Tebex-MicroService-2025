08c6ff6c26aea138504e3ec1033d06d7bed41405
vC3sar <cesarvaz.e7@gmail.com>
2025-06-13 23:58:36 -0600

    changes for proxy compatibility

--- handlers/from.js
+++ handlers/from.js
@@ -15,19 +15,36 @@ const logger = winston.createLogger({
         new winston.transports.Console() 
     ]
 });
+
+// Función para verificar si una IP es interna (Docker)
+function isDockerIP(ip) {
+    return /^172\./.test(ip) || /^192\./.test(ip) || /^10\./.test(ip);
+}
+
+// Función para obtener la IP real
+function getClientIP(req) {
+    const realIP = req.headers['cf-connecting-ip'] || req.socket.remoteAddress;
+    return isDockerIP(realIP) ? req.headers['cf-connecting-ip'] : realIP;
+}
+
 router.use('/', function (req, res, next) {
     if (req.path === '/favicon.ico') return res.redirect(api.favicon_url);
-    var ip = req.socket.remoteAddress;
-    // Update log
-    logger.info('Request received from: ' + ip)
-    if (debug) { console.log(`${colors.gray(`Nueva venta, debug mode on || from: ${ip}`)}`); }
 
-    if (ip === '::ffff:18.209.80.3' || ip === '::ffff:54.87.231.232') { next(); }
-    else {
+    const ip = getClientIP(req);  // Obtener la IP real
+    // Actualizar log
+    logger.info('Request received from: ' + ip);
+
+    if (debug) { 
+        console.log(`${colors.gray(`Nueva venta, debug mode on || from: ${ip}`)}`); 
+    }
+
+    // Verificar si la IP es una de las autorizadas
+    if (ip === '::ffff:18.209.80.3' || ip === '::ffff:54.87.231.232') { 
+        next(); 
+    } else {
         console.log(colors.red(`Bad Request from: ${ip}`));
         return res.status(403).jsonp({ error: 'Not authorized' });
     }
 });
 
 module.exports = router;
-
